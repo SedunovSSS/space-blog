8a818fc8d3921853cc82467e51877e4da9473390
SedunovSSS <sedunov308@gmail.com>
2023-05-07 06:14:54 +0500

    space-blog

--- main/urls.py
+++ main/urls.py
@@ -3,12 +3,12 @@ from .views import *
 
 urlpatterns = [
     path('', main_view),
-    path('login', login_frontend),
+    path('login/', login_frontend),
     path('login_backend', login_backend),
-    path('logout', logout_backend),
-    path('register', register_frontend),
+    path('logout/', logout_backend),
+    path('register/', register_frontend),
     path('register_backend', register_backend),
-    path('add_post', add_frontend),
+    path('add_post/', add_frontend),
     path('add_backend', add_backend),
     path('view', view),
 ]
